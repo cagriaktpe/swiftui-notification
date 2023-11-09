5841062799060f88cb3b5bf84ab3b9fa29bcecff
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-11-09 10:07:22 +0300

    Initial Commit

---
+++ SwiftUI-notification/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-notification
+//
+//  Created by Samet Çağrı Aktepe on 9.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-notification/SwiftUI_notificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_notificationApp.swift
+//  SwiftUI-notification
+//
+//  Created by Samet Çağrı Aktepe on 9.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_notificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
